a6e1c6ac9091d0948ec0f896abac817002082bb8
Franklin <franklin@learniosfreecoms-mac.local>
2016-09-10 17:15:50 -0400

    Initial Commit

---
+++ Add Delete Item App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Add Delete Item App
+//
+//  Created by Franklin on 2016-09-10.
+//  Copyright © 2016 Kim Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
